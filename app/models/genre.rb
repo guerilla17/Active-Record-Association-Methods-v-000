78072c8aa06dcfde435766bae81c8e0e60af2b8f
Gigi M <mmgamao1@gmail.com>
2019-07-29 16:56:05 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artist.all.names
+    self.artists.names.all
   end
 end
